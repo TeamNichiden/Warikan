80ee9439f88f9bc2d785ba13be64c39b98dd2dec
Saikaku <grnmnsta@gmail.com>
2025-05-02 14:58:24 +0900

    Init Project

---
+++ Grouppy/GrouppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrouppyApp.swift
+//  Grouppy
+//
+//  Created by cmStudent on 2025/05/02.
+//
+
+import SwiftUI
+
+@main
+struct GrouppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
