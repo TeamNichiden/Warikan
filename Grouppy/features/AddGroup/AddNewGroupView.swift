102173f9089cc6f2c6bf30662a6863844baa8977
Saikaku <grnmnsta@gmail.com>
2025-05-13 11:14:34 +0900

    グループ作成画面の仕様とカレンダーのサンプルシートを作成

---
+++ Grouppy/features/AddGroup/AddNewGroupView.swift
@@ -0,0 +1,98 @@
+//
+//  AddNewGroupView.swift
+//  Grouppy
+//
+//  Created by cmStudent on 2025/05/10.
+//
+
+import SwiftUI
+
+struct AddNewGroupView: View {
+    @Environment(\.dismiss) private var dismiss
+    @StateObject var vm = AddNewGroupViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("グループ作成")
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
+            groupInfoRow(title: "グループ名", message: "グループ名を入力", inputMessage: $vm.mockGroup.groupName)
+            groupInfoRow(title: "メモ", message: "詳細情報を入力(任意)", inputMessage: $vm.mockGroup.groupMemo)
+            groupInfoRow(title: "日時", message: "", inputMessage: $vm.mockGroup.dateStr, isInputText: true, btnIcon: "calendar", action: vm.showCalendar)
+            groupInfoRow(title: "場所", message: "場所を選択または入力", inputMessage: $vm.mockGroup.palce, btnIcon: "location", action: vm.showMap)
+            
+            // MARK: メンバーリスト
+            VStack(alignment: .leading) {
+                Text("メンバー")
+                    .fontWeight(.bold)
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 24))
+                    }
+                    Text("メンバーを追加")
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+            }
+            .frame(maxWidth: .infinity)
+            
+            Spacer()
+            Button {
+                vm.isShowGroup = true
+            } label: {
+                Text("グループを作成")
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+        .onAppear() {
+            vm.initialDate()
+        }
+        .sheet(isPresented: $vm.showDatePicker) {
+            DatePickerSheetView(vm: vm)
+        }
+    }
+}
+
+extension AddNewGroupView {
+    func groupInfoRow(
+        title: String,
+        message: String,
+        inputMessage: Binding<String>,
+        isInputText: Bool = false,
+        btnIcon: String? = nil,
+        action: (() -> Void)? = nil
+    ) -> some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .fontWeight(.bold)
+            HStack {
+                TextField(message,text: inputMessage)
+                    .disabled(isInputText)
+                if let btnIcon, let action {
+                    Button(action: action) {
+                        Image(systemName: btnIcon)
+                    }
+                }
+            }
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+        }
+    }
+}
+
+#Preview {
+    AddNewGroupView()
+}
