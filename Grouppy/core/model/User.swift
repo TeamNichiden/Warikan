335a1c8857e6f026a476d109c7d9c45b78b8873a
dlxsmd <dlxsmd@icloud.com>
2025-05-16 14:22:55 +0900

    userDataModel変更

--- Grouppy/core/model/User.swift
+++ Grouppy/core/model/User.swift
@@ -4,7 +4,7 @@ struct User: Identifiable, Codable, Equatable {
   let id: String  // ユーザーUUID
   var name: String
   var email: String
-  var userId: String?
+  var userId: String
   var iconUrl: URL?
   // 必要に応じて追加フィールド
 }
