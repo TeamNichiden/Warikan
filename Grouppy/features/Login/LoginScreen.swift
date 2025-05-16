e03d881cdfe69aec7b3b31d96b1a6bca6c679f34
dlxsmd <dlxsmd@icloud.com>
2025-05-16 11:18:11 +0900

    一旦不要そうなコードを削除

--- Grouppy/features/Login/LoginScreen.swift
+++ Grouppy/features/Login/LoginScreen.swift
@@ -8,16 +8,11 @@
 import SwiftUI
 
 struct LoginScreen: View {
-    @State private var showHomeView:Bool = false
     var body: some View {
         VStack {
             Text("ホーム画面へ")
                 .onTapGesture {
-                    showHomeView = true
                 }
-        }
-        .fullScreenCover(isPresented: $showHomeView) {
-            HomeView()
         }
     }
 }
