d25bd8424e7126ca7b52d349953b5a40e3f16533
dlxsmd <dlxsmd@icloud.com>
2025-05-27 14:27:53 +0900

    モディファイアの重複箇所の削除

--- Grouppy/app/GrouppyApp.swift
+++ Grouppy/app/GrouppyApp.swift
@@ -13,51 +13,43 @@ struct GrouppyApp: App {
   @StateObject private var router = NavigationRouter()
 
   // BottomBarを表示したいRouteを列挙
-    private let tabRoutes: [Route] = [.home, .eventList, .history, .setting]
+  private let tabRoutes: [Route] = [.home, .eventList, .history, .setting]
 
   var body: some Scene {
     WindowGroup {
       VStack {
         NavigationStack(path: $router.path) {
           RootView()
-            .environmentObject(auth)
-            .environmentObject(router)
             .navigationDestination(for: Route.self) { route in
               switch route {
               case .home:
                 HomeView()
-                  .environmentObject(router)
                   .toolbar(.hidden, for: .navigationBar)
               case .editProfile:
                 UserProfileView()
-                  .environmentObject(router)
               case .addEvent:
                 AddNewEventView()
-                  .environmentObject(router)
               case .eventList:
                 EventListView()
-                  .environmentObject(router)
                   .toolbar(.hidden, for: .navigationBar)
               case .event(let id):
                 EventInfoView(eventId: id)
-                  .environmentObject(router)
               case .setting:
-                  EmptyView()
-                  .environmentObject(router)
+                EmptyView()
                   .toolbar(.hidden, for: .navigationBar)
               case .history:
                 EmptyView()
-                  .environmentObject(router)
                   .toolbar(.hidden, for: .navigationBar)
               }
             }
         }
         if isShouldShowBottomBar {
           BottomNavBar(selected: router.path.last ?? .home)
-            .environmentObject(router)
             .transition(.move(edge: .bottom))
         }
       }
+      .environmentObject(auth)
+      .environmentObject(router)
     }
   }
 
