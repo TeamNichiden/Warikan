1e3381e47ff4f2e8f04b337161df2e730ffbf964
Saikaku <grnmnsta@gmail.com>
2025-06-06 16:36:56 +0900

    HomeViewModelにユーザー情報更新機能を追加

--- Grouppy/features/Home/HomeViewModel.swift
+++ Grouppy/features/Home/HomeViewModel.swift
@@ -1,20 +1,46 @@
-//
-//  HomeViewModel.swift
-//  Grouppy
-//
-//  Created by cmStudent on 2025/05/02.
-//
-
 import SwiftUI
+import SwiftData
 
+@MainActor
 class HomeViewModel: ObservableObject {
     @Published var user: AppUser
+    private var userDataService: UserDataService
     
-    init() {
-        self.user = MockData.authUser
+    init(userDataService: UserDataService) {
+        self.userDataService = userDataService
+        
+        // 如果没有保存的用户，使用 MockData
+        if let savedUser = userDataService.currentUser {
+            self.user = savedUser
+        } else {
+            userDataService.loginWithMockDataIfNeeded()
+            self.user = userDataService.currentUser ?? AppUser(
+                name: MockData.authUser.name,
+                email: MockData.authUser.email,
+                userId: MockData.authUser.userId,
+                iconUrl: MockData.authUser.iconUrl
+            )
+        }
     }
     
     func updateUser(_ newUser: AppUser) {
         self.user = newUser
+        userDataService.updateUser(newUser)
+    }
+    
+    func updateUserWithImageData(_ imageData: Data?) {
+        user.iconData = imageData
+        userDataService.updateUser(user)
+    }
+    
+    func logout() {
+        userDataService.logout()
+    }
+    
+    func login() {
+        userDataService.loginWithMockDataIfNeeded()
+        if let currentUser = userDataService.currentUser {
+            self.user = currentUser
+        }
     }
 }
