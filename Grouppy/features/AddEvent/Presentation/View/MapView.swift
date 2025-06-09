eafc33106535fb9ca8eca89a2b4751a41f73a334
Saikaku <grnmnsta@gmail.com>
2025-06-09 13:47:47 +0900

    マップ上に位置の表示を実装

--- Grouppy/features/AddEvent/Presentation/View/MapView.swift
+++ Grouppy/features/AddEvent/Presentation/View/MapView.swift
@@ -6,10 +6,36 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    @StateObject private var locationManager = LocationManager()
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(position: $position) {
+            if locationManager.isAuthorized {
+                UserAnnotation()
+            }
+        }
+        .mapControls {
+            MapUserLocationButton()
+            MapCompass()
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            locationManager.requestPermission()
+        }
+        .alert("位置権限が必要です", isPresented: .constant(!locationManager.isAuthorized && locationManager.hasRequestedPermission)) {
+            Button("設定を開く") {
+                if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(settingsUrl)
+                }
+            }
+            Button("キャンセル", role: .cancel) { }
+        } message: {
+            Text("地図上にあなたの位置を表示するために、位置情報へのアクセスを許可してください。")
+        }
     }
 }
 
