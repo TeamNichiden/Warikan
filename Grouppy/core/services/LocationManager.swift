8871f642ecb3afe6c4c47a597e9f243fddde1958
Saikaku <grnmnsta@gmail.com>
2025-06-09 13:47:19 +0900

    位置情報を管理するクラスの実装

--- Grouppy/core/services/LocationManager.swift
+++ Grouppy/core/services/LocationManager.swift
@@ -5,4 +5,36 @@
 //  Created by cmStudent on 2025/06/09.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+import Observation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    @Published var isAuthorized = false
+    @Published var hasRequestedPermission = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        checkAuthorizationStatus()
+    }
+    
+    func requestPermission() {
+        hasRequestedPermission = true
+        manager.requestWhenInUseAuthorization()
+    }
+    
+    private func checkAuthorizationStatus() {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            isAuthorized = true
+        default:
+            isAuthorized = false
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkAuthorizationStatus()
+    }
+}
